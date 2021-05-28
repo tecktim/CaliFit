c31a30b32ca23cc8d304e9f7429356705179b9ea
Tim Friedrich <tifri99@web.de>
2021-05-28 13:12:03 +0200

    Exercise und Workout Activity fortgeführt, es gibt noch einen Fehler beim zurückgeben der exercises

--- app/src/main/java/com/example/CaliFit/ExerciseActivity.java
+++ app/src/main/java/com/example/CaliFit/ExerciseActivity.java
@@ -1,16 +1,18 @@
 package com.example.CaliFit;
 
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class ExerciseActivity extends AppCompatActivity implements ExerciseActivityPresenter.ViewInterface{
     Exercise thisExercise;
+    ArrayList<Exercise> exercises = new ArrayList<>();
 
     ExerciseActivityPresenter exerciseActivityPresenter;
     @Override
@@ -23,33 +25,145 @@ public class ExerciseActivity extends AppCompatActivity implements ExerciseActiv
 
         TextView namePreviewExercise = (TextView) findViewById(R.id.namePreviewExercise);
         //Set the top name TextView to push/pull/legs/core
-        namePreviewExercise.setText(receivedIntent.getCharSequenceExtra(receivedIntent.EXTRA_TEXT + " Exercises"));
+        namePreviewExercise.setText(receivedIntent.getStringExtra(receivedIntent.EXTRA_TEXT + " Exercises"));
 
 
+        switch (receivedIntent.getStringExtra(receivedIntent.EXTRA_TEXT)){
+            case "Push":
+                Button addButtonPushUpEasy = findViewById(R.id.exAdd1);
+                TextView textPushUpEasy = findViewById(R.id.exDescription1);
+                textPushUpEasy.setText("PushUpEasy text description");
+                addButtonPushUpEasy.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Knee Push up", Exercise.Category.Push);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonPushUp = findViewById(R.id.exAdd2);
+                TextView textPushUp = findViewById(R.id.exDescription1);
+                textPushUp.setText("PushUp text description");
+                addButtonPushUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Normal Push up", Exercise.Category.Push);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonPushUpArcher = findViewById(R.id.exAdd3);
+                TextView textPushUpArcher = findViewById(R.id.exDescription1);
+                textPushUpArcher.setText("PushUpArcher text description");
+                addButtonPushUpArcher.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Archer Push up", Exercise.Category.Push);
+                        exercises.add(thisExercise);
+                    }
+                });
 
+            case "Pull":
+                Button addButtonTableRow = findViewById(R.id.exAdd1);
+                TextView textTableRow = findViewById(R.id.exDescription1);
+                textTableRow.setText("TableRow text description");
+                addButtonTableRow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Table/Chair Row", Exercise.Category.Pull);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonTowelRow = findViewById(R.id.exAdd2);
+                TextView textTowelRow = findViewById(R.id.exDescription1);
+                textTowelRow.setText("TowelRow text description");
+                addButtonTowelRow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Towel Row", Exercise.Category.Pull);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonSuperman = findViewById(R.id.exAdd3);
+                TextView textSuperman = findViewById(R.id.exDescription1);
+                textSuperman.setText("Superman text description");
+                addButtonSuperman.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Supermans", Exercise.Category.Pull);
+                        exercises.add(thisExercise);
+                    }
+                });
 
+            case "Legs":
+                Button addButtonSquat = findViewById(R.id.exAdd1);
+                TextView textSquat = findViewById(R.id.exDescription1);
+                textSquat.setText("The squat is the holy grail of legs exercises");
+                addButtonSquat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Table/Chair Row", Exercise.Category.Legs);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonLowSquat = findViewById(R.id.exAdd2);
+                TextView textLowSquat = findViewById(R.id.exDescription2);
+                textLowSquat.setText("The low squat is a harder variation of the squat");
+                addButtonLowSquat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Towel Row", Exercise.Category.Legs);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonLunge = findViewById(R.id.exAdd3);
+                TextView textLunge = findViewById(R.id.exDescription3);
+                textLunge.setText("The lunge emphasises single leg strength and hip flexibility");
+                addButtonLunge.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Supermans", Exercise.Category.Legs);
+                        exercises.add(thisExercise);
+                    }
+                });
 
-        Button addButtonPushUp = findViewById(R.id.addButtonPushup);
-        addButtonPushUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                thisExercise = new Exercise("Push up", Exercise.Category.Push);
-
-            }
-        });
-
-
-
-
+            case "Core":
+                Button addButtonCrunch = findViewById(R.id.exAdd1);
+                TextView textCrunch = findViewById(R.id.exDescription1);
+                textCrunch.setText("Crunch text description");
+                addButtonCrunch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Crunch", Exercise.Category.Core);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonLegRaise = findViewById(R.id.exAdd2);
+                TextView textLegRaise = findViewById(R.id.exDescription2);
+                textLegRaise.setText("Leg Raise text description");
+                addButtonLegRaise.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("Leg Raises", Exercise.Category.Core);
+                        exercises.add(thisExercise);
+                    }
+                });
+                Button addButtonVUp = findViewById(R.id.exAdd3);
+                TextView textVUp = findViewById(R.id.exDescription3);
+                textVUp.setText("V-Up text description");
+                addButtonVUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        thisExercise = new Exercise("V-Ups", Exercise.Category.Core);
+                        exercises.add(thisExercise);
+                    }
+                });
+        }
     }
 
     @Override
     public void finish() {
-        System.out.println(thisExercise.name);
         Intent toGive = new Intent();
-        toGive.putExtra("Exercise", thisExercise);
+        toGive.putExtra("Exercises", exercises);
         setResult(RESULT_OK, toGive);
-
         super.finish();
     }
 
--- app/src/main/java/com/example/CaliFit/WorkoutActivity.java
+++ app/src/main/java/com/example/CaliFit/WorkoutActivity.java
@@ -1,19 +1,16 @@
 package com.example.CaliFit;
 
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class WorkoutActivity extends AppCompatActivity implements WorkoutActivityPresenter.ViewInterface{
 
@@ -80,16 +77,59 @@ public class WorkoutActivity extends AppCompatActivity implements WorkoutActivit
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent toReceive) {
-        if (resultCode == RESULT_OK && requestCode == 1){
-            Exercise toAdd = (Exercise)toReceive.getSerializableExtra("Exercise");
-            workoutActivityPresenter.getModel().addListItem(toAdd);
+        if (resultCode == RESULT_OK){
+            switch(requestCode) {
+                case 1:Exercise pushToAdd = (Exercise)toReceive.getSerializableExtra("Exercise");
+                workoutActivityPresenter.getModel().addListItem(pushToAdd);
+                case 2:Exercise pullToAdd = (Exercise)toReceive.getSerializableExtra("Exercise");
+                workoutActivityPresenter.getModel().addListItem(pullToAdd);
+                case 3:Exercise legsToAdd = (Exercise)toReceive.getSerializableExtra("Exercise");
+                workoutActivityPresenter.getModel().addListItem(legsToAdd);
+                case 4:Exercise coreToAdd = (Exercise)toReceive.getSerializableExtra("Exercise");
+                workoutActivityPresenter.getModel().addListItem(coreToAdd);
+                default:
+                    return;
+            }
         }
+
     }
 
     @Override
     public void onRestart(){
         super.onRestart();
-        TextView pushExercise0 = findViewById(R.id.push_exercise_text);
-        pushExercise0.setText(workoutActivityPresenter.getModel().PushList.get(0).name);
+        Iterator<Exercise> itr = workoutActivityPresenter.getModel().PushList.iterator();
+        TextView pushExercise1 = findViewById(R.id.push_exercise_text1);
+        TextView pushExercise2 = findViewById(R.id.push_exercise_text2);
+        TextView pushExercise3 = findViewById(R.id.push_exercise_text3);
+        TextView pullExercise1 = findViewById(R.id.pull_exercise_text1);
+        TextView pullExercise2 = findViewById(R.id.pull_exercise_text2);
+        TextView pullExercise3 = findViewById(R.id.pull_exercise_text3);
+        TextView coreExercise1 = findViewById(R.id.core_exercise_text1);
+        TextView coreExercise2 = findViewById(R.id.core_exercise_text2);
+        TextView coreExercise3 = findViewById(R.id.core_exercise_text3);
+        TextView legsExercise1 = findViewById(R.id.legs_exercise_text1);
+        TextView legsExercise2 = findViewById(R.id.legs_exercise_text2);
+        TextView legsExercise3 = findViewById(R.id.legs_exercise_text3);
+        ArrayList<TextView> nameBoxes = new ArrayList<>();
+        nameBoxes.add(pushExercise1); nameBoxes.add(pushExercise2); nameBoxes.add(pushExercise3); nameBoxes.add(pullExercise1); nameBoxes.add(pullExercise2); nameBoxes.add(pullExercise3); nameBoxes.add(legsExercise1); nameBoxes.add(legsExercise2); nameBoxes.add(legsExercise3); nameBoxes.add(coreExercise1); nameBoxes.add(coreExercise2); nameBoxes.add(coreExercise3);
+
+        for(int i = 0; i < workoutActivityPresenter.getModel().PushList.size(); i++){
+            nameBoxes.get(i).setText(workoutActivityPresenter.getModel().PushList.get(i).name);
+            nameBoxes.get(i).setVisibility(View.VISIBLE);
+        }
+        for(int i = 0; i < workoutActivityPresenter.getModel().PullList.size(); i++){
+            nameBoxes.get(i).setText(workoutActivityPresenter.getModel().PullList.get(i).name);
+            nameBoxes.get(i).setVisibility(View.VISIBLE);
+        }
+        for(int i = 0; i < workoutActivityPresenter.getModel().LegsList.size(); i++){
+            nameBoxes.get(i).setText(workoutActivityPresenter.getModel().LegsList.get(i).name);
+            nameBoxes.get(i).setVisibility(View.VISIBLE);
+        }
+        for(int i = 0; i < workoutActivityPresenter.getModel().CoreList.size(); i++){
+            nameBoxes.get(i).setText(workoutActivityPresenter.getModel().CoreList.get(i).name);
+            nameBoxes.get(i).setVisibility(View.VISIBLE);
+        }
+
+
     }
 }
